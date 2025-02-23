dc9e57537e9d628ea6d56c9401fd3f92d2f551a2
Satwik M Badiger <117066242+Satwikmbadiger@users.noreply.github.com>
2025-02-23 11:22:26 +0530

    Update index.js

--- product/static/index.js
+++ product/static/index.js
@@ -10,13 +10,14 @@ import { getAuth,
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAu_fXf_EoNpp10WRs7xc2F3fKbla7WAS0",
-    authDomain: "simple-firebase-91658.firebaseapp.com",
-    projectId: "simple-firebase-91658",
-    storageBucket: "simple-firebase-91658.appspot.com",
-    messagingSenderId: "424748845206",
-    appId: "1:424748845206:web:966c5ed3fc459277411ab7",
-    measurementId: "G-H4WKK504Y4"
+    aapiKey: "AIzaSyBuZ-Z-G_wjKm1yxzz0O26IzEBMXfURpWg",
+  authDomain: "med-app-2fde8.firebaseapp.com",
+  databaseURL: "https://med-app-2fde8-default-rtdb.firebaseio.com",
+  projectId: "med-app-2fde8",
+  storageBucket: "med-app-2fde8.firebasestorage.app",
+  messagingSenderId: "712372987359",
+  appId: "1:712372987359:web:7d6b205ec366c0d4eed06a",
+  measurementId: "G-JLBSPB84R1"
   };
 
   // Initialize Firebase
